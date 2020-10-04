3cf0837d1a9f125e23e15ba1bd0c1cb65c28c94a
Grant <granthend00@gmail.com>
2020-10-03 18:38:27 -0700

    add interactive hovering for data

--- data-challenge/04-output/primary_chart_script.R
+++ data-challenge/04-output/primary_chart_script.R
@@ -2,12 +2,13 @@
 rm(list=ls(all=TRUE))
 
 ## Set working directory
-setwd("~/data-challenge/02-clean-data")
+setwd("C:/Users/Grant/Documents/GitHub/prop18/data-challenge/02-clean-data")
 
 ## Load packages
 library(tidyverse)
+library(plotly)
 
-youth_vote <- read_csv("clean_youth_vote_200928.csv")
+youth_vote <- read_csv("clean_youth_vote_2016.csv")
 
 states = c("CT", "CO", "DE", "DC", "IL", "IN", "KY", "ME", "MA", "MI",
            "NE", "NM", "NC", "OH", "SC", "UT", "VT", "VA", "WV")
@@ -15,8 +16,16 @@ states = c("CT", "CO", "DE", "DC", "IL", "IN", "KY", "ME", "MA", "MI",
 plot <- ggplot(data=youth_vote) +
   xlab("Voter Registration Rate") +
   ylab("Turnout Rate") +
+  geom_smooth(mapping=aes(x=register, y=turnout)) +
   geom_point(mapping=aes(x=register, y=turnout, 
-                         color = ifelse(state_code %in% states, "17-Year-Old Primary Voting", "None")), size=5) +
-  scale_color_manual(name="State Allows 17-Year-Olds to Vote in Primaries?",values=c("green","red"),labels=c("Yes","No"))
+                         color = ifelse(state_code %in% states, "17-Year-Old Primary Voting", "None"), text=paste('State: ', state_code), size=5)) +
+  scale_color_manual(name="State lets 17 year olds vote \n in primaries?",values=c("green","black"),labels=c("Yes","No"))
 
-plot
+ggplotly(plot, tooltip=c("text"))
+
+plot2 <- ggplot(data=youth_vote) +
+  xlab("Election Law Score") +
+  ylab("Turnout Rate") +
+  geom_point(mapping=aes(x=elect_law_score, y=turnout))
+
+ggplotly(plot2)
